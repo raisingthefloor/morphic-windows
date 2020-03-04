2867e5aae7f7f6a84db49f7e5e76f1d377fd0c0a
Owen Shaw <owen@owenshaw.com>
2020-03-04 08:20:42 -0800

    adding library projects

---
+++ MorphicCore/Preferences.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MorphicCore
+{
+    public class Preferences
+    {
+    }
+}
---
+++ MorphicSettings/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MorphicSettings
+{
+    public class Class1
+    {
+    }
+}
