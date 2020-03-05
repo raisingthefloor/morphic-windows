65573aa9857a385a0cebef030be6b0eebd2f2d6f
Owen Shaw <owen@owenshaw.com>
2020-03-05 08:07:38 -0800

    run without using MainWindow

--- MorphicWin/App.xaml.cs
+++ MorphicWin/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace MorphicWin
 {
@@ -7,5 +8,66 @@ namespace MorphicWin
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            CreateNotifyIcon();
+            Settings.Default.PropertyChanged += OnSettingChanged;
+            if (Settings.Default.UserId == "")
+            {
+                var configurator = new MorphicConfigurator();
+                configurator.Show();
+                configurator.Activate();
+            }
+        }
+
+        private void OnSettingChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "UserId" && Settings.Default.UserId != "" && quickStrip == null)
+            {
+                ToggleQuickStrip();
+            }
+        }
+
+        private System.Windows.Forms.NotifyIcon notifyIcon = null;
+
+        private void CreateNotifyIcon()
+        {
+            notifyIcon = new System.Windows.Forms.NotifyIcon();
+            notifyIcon.Click += OnNotifyIconClicked;
+            notifyIcon.Icon = MorphicWin.Properties.Resources.Icon;
+            notifyIcon.Text = "Morphic Quick Strip";
+            notifyIcon.Visible = true;
+        }
+
+        private void OnNotifyIconClicked(object sender, EventArgs e)
+        {
+            ToggleQuickStrip();
+        }
+
+        private Window quickStrip = null;
+
+        private void ToggleQuickStrip()
+        {
+            if (quickStrip != null)
+            {
+                quickStrip.Close();
+            }
+            else
+            {
+                quickStrip = new QuickStrip();
+                quickStrip.Closed += QuickStripClosed;
+                var screenSize = SystemParameters.WorkArea;
+                quickStrip.Top = screenSize.Height - quickStrip.Height;
+                quickStrip.Left = screenSize.Width - quickStrip.Width;
+                quickStrip.Show();
+                quickStrip.Activate();
+            }
+        }
+
+        private void QuickStripClosed(object sender, EventArgs e)
+        {
+            quickStrip = null;
+        }
     }
 }
