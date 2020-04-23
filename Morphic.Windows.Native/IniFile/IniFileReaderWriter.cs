026bcb87527b5d19bb9f289049a500dc35fe70ab
Christopher - RtF <58520035+christopher-rtf@users.noreply.github.com>
2020-04-23 10:51:26 -0400

    Updated copyright and funding information text

--- Morphic.Windows.Native/IniFile/IniFileReaderWriter.cs
+++ Morphic.Windows.Native/IniFile/IniFileReaderWriter.cs
@@ -1,14 +1,25 @@
-﻿//
-// IniFileReaderWriter.cs
-// Morphic support library for Windows
+﻿// Copyright 2020 Raising the Floor - International
 //
-// Copyright © 2020 Raising the Floor -- US Inc. All rights reserved.
+// Licensed under the New BSD license. You may not use this file except in
+// compliance with this License.
 //
-// The R&D leading to these results received funding from the
-// Department of Education - Grant H421A150005 (GPII-APCP). However,
-// these results do not necessarily represent the policy of the
-// Department of Education, and you should not assume endorsement by the
-// Federal Government.
+// You may obtain a copy of the License at
+// https://github.com/GPII/universal/blob/master/LICENSE.txt
+//
+// The R&D leading to these results received funding from the:
+// * Rehabilitation Services Administration, US Dept. of Education under 
+//   grant H421A150006 (APCP)
+// * National Institute on Disability, Independent Living, and 
+//   Rehabilitation Research (NIDILRR)
+// * Administration for Independent Living & Dept. of Education under grants 
+//   H133E080022 (RERC-IT) and H133E130028/90RE5003-01-00 (UIITA-RERC)
+// * European Union's Seventh Framework Programme (FP7/2007-2013) grant 
+//   agreement nos. 289016 (Cloud4all) and 610510 (Prosperity4All)
+// * William and Flora Hewlett Foundation
+// * Ontario Ministry of Research and Innovation
+// * Canadian Foundation for Innovation
+// * Adobe Foundation
+// * Consumer Electronics Association Foundation
 
 using System;
 using System.Runtime.InteropServices;
