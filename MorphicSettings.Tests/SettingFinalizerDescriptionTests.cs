31e96fc2aa1bea72f90e7f35c3aa2c46998b7e19
James Vanderheiden <james.v@digipen.edu>
2020-05-15 06:06:02 -0700

    finished data model testing

---
+++ MorphicSettings.Tests/SettingFinalizerDescriptionTests.cs
@@ -0,0 +1,67 @@
+﻿using MorphicCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using Xunit;
+
+namespace MorphicSettings.Tests
+{
+    public class SettingFinalizerDescriptionTests
+    {
+        [Fact]
+        public void TestJsonDeserializeUnknown()
+        {
+            var options = new JsonSerializerOptions();
+            options.Converters.Add(new JsonElementInferredTypeConverter());
+            options.Converters.Add(new SettingFinalizerDescription.JsonConverter());
+
+            var json = JsonSerializer.Serialize(new Dictionary<string, object?>()
+            {
+                { "type", "invalid" }
+            });
+            var finalizer = JsonSerializer.Deserialize<SettingFinalizerDescription>(json, options);
+            Assert.NotNull(finalizer);
+            Assert.Equal(SettingFinalizerDescription.HandlerKind.Unknown, finalizer.Kind);
+        }
+
+        [Theory]
+        [InlineData("setCursors", SystemParametersInfo.Action.SetCursors, null, null, true)]
+        [InlineData("setCursors", SystemParametersInfo.Action.SetCursors, false, false, true)]
+        [InlineData("setCursors", SystemParametersInfo.Action.SetCursors, false, true, true)]
+        [InlineData("setCursors", SystemParametersInfo.Action.SetCursors, true, false, true)]
+        [InlineData("setCursors", SystemParametersInfo.Action.SetCursors, true, true, true)]
+        [InlineData("shenanigans", SystemParametersInfo.Action.SetCursors, false, false, false)]
+        public void TestJsonDeserializeSPI(string actionString, SystemParametersInfo.Action action, bool? sendChange, bool? updateUserProfile, bool success)
+        {
+            var options = new JsonSerializerOptions();
+            options.Converters.Add(new JsonElementInferredTypeConverter());
+            options.Converters.Add(new SettingFinalizerDescription.JsonConverter());
+
+            var json = JsonSerializer.Serialize(new Dictionary<string, object?>()
+            {
+                { "type", "com.microsoft.windows.systemParametersInfo" },
+                { "action", actionString },
+                { "send_change", sendChange },
+                { "update_user_profile", updateUserProfile }
+            });
+            var finalizer = JsonSerializer.Deserialize<SettingFinalizerDescription>(json, options);
+            Assert.NotNull(finalizer);
+            if (success)
+            {
+                Assert.Equal(SettingFinalizerDescription.HandlerKind.SystemParametersInfo, finalizer.Kind);
+                Assert.IsType<SystemParametersInfoSettingFinalizerDescription>(finalizer);
+                SystemParametersInfoSettingFinalizerDescription spi = (SystemParametersInfoSettingFinalizerDescription)finalizer;
+                Assert.Equal(action, spi.Action);
+                if (sendChange != null)
+                    Assert.Equal(sendChange, spi.SendChange);
+                if (updateUserProfile != null)
+                    Assert.Equal(updateUserProfile, spi.UpdateUserProfile);
+            }
+            else
+            {
+                Assert.Equal(SettingFinalizerDescription.HandlerKind.Unknown, finalizer.Kind);
+            }
+        }
+    }
+}
---
+++ MorphicSettings.Tests/SettingHandlerDescriptionTests.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.Win32;
+using MorphicCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using Xunit;
+
+namespace MorphicSettings.Tests
+{
+    public class SettingHandlerDescriptionTests
+    {
+        [Fact]
+        public void TestJsonDeserializeUnknown()
+        {
+            var options = new JsonSerializerOptions();
+            options.Converters.Add(new JsonElementInferredTypeConverter());
+            options.Converters.Add(new SettingHandlerDescription.JsonConverter());
+
+            var json = JsonSerializer.Serialize(new Dictionary<string, object?>()
+            {
+                { "type", "invalid" }
+            });
+            var handler = JsonSerializer.Deserialize<SettingHandlerDescription>(json, options);
+            Assert.NotNull(handler);
+            Assert.Equal(SettingHandlerDescription.HandlerKind.Unknown, handler.Kind);
+        }
+
+        [Theory]
+        [InlineData("ayy", "lmao", true)]
+        [InlineData(null, "lmao", true)]
+        [InlineData("ayy", null, true)]
+        [InlineData(null, null, true)]
+        public void TestJsonDeserializeClient(string? solution, string? preference, bool success)
+        {
+            var options = new JsonSerializerOptions();
+            options.Converters.Add(new JsonElementInferredTypeConverter());
+            options.Converters.Add(new SettingHandlerDescription.JsonConverter());
+
+            var json = JsonSerializer.Serialize(new Dictionary<string, object?>()
+            {
+                { "type", "org.raisingthefloor.morphic.client" },
+                { "solution", solution },
+                { "preference", preference }
+            });
+            var handler = JsonSerializer.Deserialize<SettingHandlerDescription>(json, options);
+            Assert.NotNull(handler);
+            if (success)
+            {
+                Assert.Equal(SettingHandlerDescription.HandlerKind.Client, handler.Kind);
+                Assert.IsType<ClientSettingHandlerDescription>(handler);
+                ClientSettingHandlerDescription client = (ClientSettingHandlerDescription)handler;
+                Assert.Equal(solution, client.Key.Solution);
+                Assert.Equal(preference, client.Key.Preference);
+            }
+            else
+            {
+                Assert.Equal(SettingHandlerDescription.HandlerKind.Unknown, handler.Kind);
+            }
+        }
+
+        [Theory]
+        [InlineData("thekey", "thevalue", "binary", RegistryValueKind.Binary, true)]
+        public void TestJsonDeserializeRegistry(string? keyName, string? valueName, string? valueType, RegistryValueKind valueKind, bool success)
+        {
+            var options = new JsonSerializerOptions();
+            options.Converters.Add(new JsonElementInferredTypeConverter());
+            options.Converters.Add(new SettingHandlerDescription.JsonConverter());
+
+            var json = JsonSerializer.Serialize(new Dictionary<string, object?>()
+            {
+                { "type", "com.microsoft.windows.registry" },
+                { "key_name", keyName },
+                { "value_name", valueName },
+                { "value_type", valueType }
+            });
+            var handler = JsonSerializer.Deserialize<SettingHandlerDescription>(json, options);
+            Assert.NotNull(handler);
+            if (success)
+            {
+                Assert.Equal(SettingHandlerDescription.HandlerKind.Registry, handler.Kind);
+                Assert.IsType<RegistrySettingHandlerDescription>(handler);
+                RegistrySettingHandlerDescription registry = (RegistrySettingHandlerDescription)handler;
+                Assert.Equal(keyName, registry.KeyName);
+                Assert.Equal(valueName, registry.ValueName);
+                Assert.Equal(valueKind, registry.ValueKind);
+            }
+            else
+            {
+                Assert.Equal(SettingHandlerDescription.HandlerKind.Unknown, handler.Kind);
+            }
+        }
+
+        [Theory]
+        [InlineData(null, null, null, true)]
+        public void TestJsonDeserializeINI(string? filename, string? section, string? key, bool success)
+        {
+            var options = new JsonSerializerOptions();
+            options.Converters.Add(new JsonElementInferredTypeConverter());
+            options.Converters.Add(new SettingHandlerDescription.JsonConverter());
+
+            var json = JsonSerializer.Serialize(new Dictionary<string, object?>()
+            {
+                { "type", "com.microsoft.windows.ini" },
+                { "filename", filename },
+                { "section", section },
+                { "key", key }
+            });
+            var handler = JsonSerializer.Deserialize<SettingHandlerDescription>(json, options);
+            Assert.NotNull(handler);
+            if (success)
+            {
+                Assert.Equal(SettingHandlerDescription.HandlerKind.Ini, handler.Kind);
+                Assert.IsType<IniSettingHandlerDescription>(handler);
+                IniSettingHandlerDescription ini = (IniSettingHandlerDescription)handler;
+                Assert.Equal(filename, ini.Filename);
+                Assert.Equal(section, ini.Section);
+                Assert.Equal(key, ini.Key);
+            }
+            else
+            {
+                Assert.Equal(SettingHandlerDescription.HandlerKind.Unknown, handler.Kind);
+            }
+        }
+
+        [Theory]
+        [InlineData(null, true)]
+        public void TestJsonDeserializeSystem( string? settingId, bool success)
+        {
+            var options = new JsonSerializerOptions();
+            options.Converters.Add(new JsonElementInferredTypeConverter());
+            options.Converters.Add(new SettingHandlerDescription.JsonConverter());
+
+            var json = JsonSerializer.Serialize(new Dictionary<string, object?>()
+            {
+                { "type", "com.microsoft.windows.system" },
+                { "setting_id", settingId }
+            });
+            var handler = JsonSerializer.Deserialize<SettingHandlerDescription>(json, options);
+            Assert.NotNull(handler);
+            if (success)
+            {
+                Assert.Equal(SettingHandlerDescription.HandlerKind.System, handler.Kind);
+                Assert.IsType<SystemSettingHandlerDescription>(handler);
+                SystemSettingHandlerDescription system = (SystemSettingHandlerDescription)handler;
+                Assert.Equal(settingId, system.SettingId);
+            }
+            else
+            {
+                Assert.Equal(SettingHandlerDescription.HandlerKind.Unknown, handler.Kind);
+            }
+        }
+    }
+}
