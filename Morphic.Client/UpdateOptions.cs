6fa294284f810997f17c5008a98631a510413d44
Owen Shaw <owen@owenshaw.com>
2020-06-01 17:05:39 -0700

    use versioning from csproj file; add update check on launch

---
+++ Morphic.Client/UpdateOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Morphic.Client
+{
+    class UpdateOptions
+    {
+
+        public string AppCastUrl { get; set; } = "";
+    }
+}
