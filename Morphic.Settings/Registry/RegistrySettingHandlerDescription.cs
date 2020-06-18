e71a3b2d725969a0d0f981653c70fd7c4721ed44
Owen Shaw <owen@owenshaw.com>
2020-06-18 15:32:33 -0700

    split out handler descriptions into subfolders

---
+++ Morphic.Settings/Registry/RegistrySettingHandlerDescription.cs
@@ -0,0 +1,89 @@
+﻿// Copyright 2020 Raising the Floor - International
+//
+// Licensed under the New BSD license. You may not use this file except in
+// compliance with this License.
+//
+// You may obtain a copy of the License at
+// https://github.com/GPII/universal/blob/master/LICENSE.txt
+//
+// The R&D leading to these results received funding from the:
+// * Rehabilitation Services Administration, US Dept. of Education under 
+//   grant H421A150006 (APCP)
+// * National Institute on Disability, Independent Living, and 
+//   Rehabilitation Research (NIDILRR)
+// * Administration for Independent Living & Dept. of Education under grants 
+//   H133E080022 (RERC-IT) and H133E130028/90RE5003-01-00 (UIITA-RERC)
+// * European Union's Seventh Framework Programme (FP7/2007-2013) grant 
+//   agreement nos. 289016 (Cloud4all) and 610510 (Prosperity4All)
+// * William and Flora Hewlett Foundation
+// * Ontario Ministry of Research and Innovation
+// * Canadian Foundation for Innovation
+// * Adobe Foundation
+// * Consumer Electronics Association Foundation
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Morphic.Core;
+using Microsoft.Win32;
+using Morphic.Settings.SystemSettings;
+using System.Linq;
+using Morphic.Settings.Files;
+
+namespace Morphic.Settings
+{
+
+    /// <summary>
+    /// A registry handler description with registry key names
+    /// </summary>
+    public class RegistrySettingHandlerDescription : SettingHandlerDescription
+    {
+
+        /// <summary>
+        /// The full key name in the registry
+        /// </summary>
+        public string KeyName;
+
+        /// <summary>
+        /// The value name within the key
+        /// </summary>
+        public string ValueName;
+
+        /// <summary>
+        /// The expected value kind
+        /// </summary>
+        public RegistryValueKind ValueKind;
+
+        /// <summary>
+        /// Create a new registry handler description
+        /// </summary>
+        /// <param name="keyName"></param>
+        /// <param name="valueName"></param>
+        /// <param name="valueKind"></param>
+        public RegistrySettingHandlerDescription(string keyName, string valueName, RegistryValueKind valueKind) : base(HandlerKind.Registry)
+        {
+            KeyName = keyName;
+            ValueName = valueName;
+            ValueKind = valueKind;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is RegistrySettingHandlerDescription other)
+            {
+                return other.KeyName == KeyName && other.ValueName == ValueName;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return KeyName.GetHashCode() ^ ValueName.GetHashCode();
+        }
+    }
+
+}
---
+++ Morphic.Settings/SystemSettings/SystemSettingHandlerDescription.cs
@@ -0,0 +1,90 @@
+﻿// Copyright 2020 Raising the Floor - International
+//
+// Licensed under the New BSD license. You may not use this file except in
+// compliance with this License.
+//
+// You may obtain a copy of the License at
+// https://github.com/GPII/universal/blob/master/LICENSE.txt
+//
+// The R&D leading to these results received funding from the:
+// * Rehabilitation Services Administration, US Dept. of Education under 
+//   grant H421A150006 (APCP)
+// * National Institute on Disability, Independent Living, and 
+//   Rehabilitation Research (NIDILRR)
+// * Administration for Independent Living & Dept. of Education under grants 
+//   H133E080022 (RERC-IT) and H133E130028/90RE5003-01-00 (UIITA-RERC)
+// * European Union's Seventh Framework Programme (FP7/2007-2013) grant 
+//   agreement nos. 289016 (Cloud4all) and 610510 (Prosperity4All)
+// * William and Flora Hewlett Foundation
+// * Ontario Ministry of Research and Innovation
+// * Canadian Foundation for Innovation
+// * Adobe Foundation
+// * Consumer Electronics Association Foundation
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Morphic.Core;
+using Microsoft.Win32;
+using Morphic.Settings.SystemSettings;
+using System.Linq;
+using Morphic.Settings.Files;
+
+namespace Morphic.Settings
+{
+
+    /// <summary>
+    /// An ini handler description that specifies which part of the ini file should be udpated
+    /// </summary>
+    public class SystemSettingHandlerDescription : SettingHandlerDescription
+    {
+
+        /// <summary>
+        /// The filename of the ini file, possibly including environmental variables
+        /// </summary>
+        public string SettingId;
+
+        /// <summary>
+        /// The kind of value expected for this setting
+        /// </summary>
+        public SystemValueKind ValueKind;
+
+        /// <summary>
+        /// Mapping from integer values to string values
+        /// </summary>
+        public string[]? IntegerMap;
+
+        /// <summary>
+        /// Mapping from string values to integer values
+        /// </summary>
+        public Dictionary<string, long>? ReverseIntegerMap;
+
+        /// <summary>
+        /// Create a new ini file handler
+        /// </summary>
+        /// <param name="settingId"></param>
+        public SystemSettingHandlerDescription(string settingId, SystemValueKind valueKind) : base(HandlerKind.System)
+        {
+            SettingId = settingId;
+            ValueKind = valueKind;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is SystemSettingHandlerDescription other)
+            {
+                return other.SettingId == SettingId;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return SettingId.GetHashCode();
+        }
+    }
+}
