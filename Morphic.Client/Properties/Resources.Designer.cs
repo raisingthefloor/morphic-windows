e0691cb8a3348de8f3e6a7dfdb9c974886e471b7
ste <git@grundell.co.uk>
2020-07-27 16:01:18 +0100

    MOR-84: Renamed read text

--- Morphic.Client/Properties/Resources.Designer.cs
+++ Morphic.Client/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Morphic.Client.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -260,7 +260,7 @@ namespace Morphic.Client.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Read Text.
+        ///   Looks up a localized string similar to Read Selected.
         /// </summary>
         internal static string QuickStrip_Reader_Title {
             get {
