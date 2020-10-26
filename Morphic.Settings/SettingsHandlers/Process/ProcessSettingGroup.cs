a7a1e572bfbba0901ee3dc3f4486208bb6bd8879
ste <git@grundell.co.uk>
2020-10-26 21:43:06 +0000

    MOR-335: Implemented process settings handler

--- Morphic.Settings/SettingsHandlers/Process/ProcessSettingGroup.cs
+++ Morphic.Settings/SettingsHandlers/Process/ProcessSettingGroup.cs
@@ -1,7 +1,15 @@
 ﻿namespace Morphic.Settings.SettingsHandlers.Process
 {
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+
     [SettingsHandlerType("process", typeof(ProcessSettingsHandler))]
     public class ProcessSettingGroup : SettingGroup
     {
+        [JsonProperty("args")]
+        public List<string> Arguments { get; private set; } = new List<string>();
+
+        [JsonProperty("env")]
+        public Dictionary<string, string> Environment { get; private set; } = new Dictionary<string, string>();
     }
 }
--- Morphic.Settings/SettingsHandlers/Process/ProcessSettingsHandler.cs
+++ Morphic.Settings/SettingsHandlers/Process/ProcessSettingsHandler.cs
@@ -2,20 +2,75 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
     using System.Threading.Tasks;
     using SolutionsRegistry;
 
     [SrService]
-    public class ProcessSettingsHandler : SettingsHandler
+    public class ProcessSettingsHandler : FixedSettingsHandler
     {
-        public override Task<Values> Get(SettingGroup settingGroup, IEnumerable<Setting> settings)
+        /// <summary>Gets the "friendly name" of a process. That is, without the path or extension.</summary>
+        private static string GetProcessName(string path)
         {
-            throw new NotImplementedException();
+            return Path.ChangeExtension(Path.GetFileName(path), null);
         }
 
-        public override Task<bool> Set(SettingGroup settingGroup, Values values)
+        private static Process[] GetProcesses(string path)
         {
-            throw new NotImplementedException();
+            string processName = GetProcessName(path);
+            return Process.GetProcessesByName(processName);
+        }
+
+        [Setter("isRunning")]
+        public Task<bool> SetRunning(Setting setting, object? newValue)
+        {
+            if (newValue is bool start)
+            {
+                Process[] processes = GetProcesses(setting.SettingGroup.Path);
+                if (start)
+                {
+                    if (processes.Length == 0)
+                    {
+                        this.StartProcess(setting);
+                    }
+                }
+                else if (processes.Length > 0)
+                {
+                    foreach (Process process in processes)
+                    {
+                        process.Kill(true);
+                    }
+                }
+            }
+
+            return Task.FromResult(true);
+        }
+
+        [Getter("isRunning")]
+        public Task<object?> GetRunning(Setting setting)
+        {
+            bool running = GetProcesses(setting.SettingGroup.Path).Length > 0;
+            return Task.FromResult<object?>(running);
+        }
+
+        private void StartProcess(Setting setting)
+        {
+            ProcessSettingGroup group = (ProcessSettingGroup)setting.SettingGroup;
+
+            ProcessStartInfo startInfo = new ProcessStartInfo(group.Path)
+            {
+                UseShellExecute = true
+            };
+
+            group.Arguments.ForEach(startInfo.ArgumentList.Add);
+            foreach (KeyValuePair<string, string> item in group.Environment)
+            {
+                startInfo.Environment.Add(item);
+            }
+
+            Process.Start(startInfo);
         }
     }
 }
