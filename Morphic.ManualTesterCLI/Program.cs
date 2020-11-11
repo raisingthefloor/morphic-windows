f044630db1ab8e798ec2fab2cb7c6dd414d8d7ed
James Vanderheiden <james.v@digipen.edu>
2020-11-11 06:58:12 -0800

    ported code over

---
+++ Morphic.ManualTesterCLI/Program.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Morphic.ManualTesterCLI
+{
+    class Program
+    {
+        static RegistryManager manager = new RegistryManager();
+        static string appname = "morphictest";
+        static async Task Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                try
+                {
+                    await manager.Load(args[0]);
+                    switch (args[1])
+                    {
+                        case "list":
+                            switch (args.Length)
+                            {
+                                case 2:
+                                    manager.List();
+                                    break;
+                                case 3:
+                                    manager.ListSpecific(args[2]);
+                                    break;
+                                default:
+                                    Console.WriteLine("[ERROR]: Incorrect number of parameters. Use: {0} <filename> list [solution]", appname);
+                                    break;
+                            }
+                            break;
+                        case "listsol":
+                            manager.ListSolutions();
+                            break;
+                        case "info":
+                            if (args.Length == 4)
+                            {
+                                manager.Info(args[2], args[3]);
+                            }
+                            else
+                            {
+                                Console.WriteLine("[ERROR]: Incorrect number of parameters. Use: {0} <filename> info <solution> <preference>", appname);
+                            }
+                            break;
+                        case "get":
+                            switch (args.Length)
+                            {
+                                case 2:
+                                    await manager.Get();
+                                    break;
+                                case 3:
+                                    await manager.Get(args[2]);
+                                    break;
+                                case 4:
+                                    await manager.Get(args[2], args[3]);
+                                    break;
+                                default:
+                                    Console.WriteLine("[ERROR]: Incorrect number of parameters. Use: {0} <filename> get [solution] [preference]", appname);
+                                    break;
+                            }
+                            break;
+                        case "set":
+                            if (args.Length == 5)
+                            {
+                                await manager.Set(args[2], args[3], args[4]);
+                            }
+                            else
+                            {
+                                Console.WriteLine("[ERROR]: Incorrect number of parameters. Use: {0} <filename> set <solution> <preference> <value>", appname);
+                            }
+                            break;
+                        case "help":
+                            helpdoc(true);
+                            break;
+                        default:
+                            Console.WriteLine("[ERROR]: Unrecognized command. Commands: list, listsol, info, get, set, help");
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("[ERROR]: Could not load file {0} as a valid solutions registry JSON file. Check filename and try again.", args[0]);
+                }
+            }
+            else if (args.Length == 1)
+            {
+                if (args[0] == "help")
+                {
+                    helpdoc(true);
+                }
+                else
+                {
+                    try
+                    {
+                        await manager.Load(args[0]);
+                        Console.Clear();
+                        Console.WriteLine("Solutions file loaded successfully.");
+                        Console.WriteLine("Welcome to the Morphic Manual Solutions Registry Tester.");
+                        Console.WriteLine("Morphic is Copyright 2020 Raising the Floor - International\n");
+                        var loop = true;
+                        while (loop)
+                        {
+                            Console.WriteLine("Please enter a command, type 'help' to list all commands:");
+                            Console.Write("> ");
+                            var line = Console.ReadLine();
+                            var nargs = line.Split(" ");
+                            if (nargs.Length > 0)
+                            {
+                                switch (nargs[0])
+                                {
+                                    case "list":
+                                        switch (nargs.Length)
+                                        {
+                                            case 1:
+                                                manager.List();
+                                                break;
+                                            case 2:
+                                                manager.ListSpecific(nargs[1]);
+                                                break;
+                                            default:
+                                                Console.WriteLine("[ERROR]: Incorrect number of parameters. Use: list [solution]");
+                                                break;
+                                        }
+                                        break;
+                                    case "listsol":
+                                        manager.ListSolutions();
+                                        break;
+                                    case "info":
+                                        if (nargs.Length == 3)
+                                        {
+                                            manager.Info(nargs[1], nargs[2]);
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("[ERROR]: Incorrect number of parameters. Use: info <solution> <preference>");
+                                        }
+                                        break;
+                                    case "get":
+                                        switch (nargs.Length)
+                                        {
+                                            case 1:
+                                                await manager.Get();
+                                                break;
+                                            case 2:
+                                                await manager.Get(nargs[1]);
+                                                break;
+                                            case 3:
+                                                await manager.Get(nargs[1], nargs[2]);
+                                                break;
+                                            default:
+                                                Console.WriteLine("[ERROR]: Incorrect number of parameters. Use: get [solution] [preference]");
+                                                break;
+                                        }
+                                        break;
+                                    case "set":
+                                        if (nargs.Length == 4)
+                                        {
+                                            await manager.Set(nargs[1], nargs[2], nargs[3]);
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("[ERROR]: Incorrect number of parameters. Use: set <solution> <preference> <value>");
+                                        }
+                                        break;
+                                    case "help":
+                                        helpdoc(false);
+                                        break;
+                                    case "quit":
+                                    case "exit":
+                                        loop = false;
+                                        break;
+                                    default:
+                                        Console.WriteLine("[ERROR]: Invalid command");
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("[ERROR]: Could not load file {0} as a valid solutions registry JSON file. Check filename and try again.", args[0]);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("[ERROR]: Valid solutions registry file path required. Use: {0} <filename>", appname);
+            }
+        }
+
+        private static void helpdoc(bool cmdline)
+        {
+            Console.Clear();
+            var header = "";
+            if (cmdline)
+            {
+                header = appname + " <filename> ";
+            }
+            Console.WriteLine("\t{0}list [solution]:", header);
+            Console.WriteLine("Lists all solutions and settings from the registry, or if provided a solution, only lists settings for that solution\n");
+            Console.WriteLine("\t{0}listsol:", header);
+            Console.WriteLine("Lists all solutions without their settings for quick lookup\n");
+            Console.WriteLine("\t{0}info <solution> <preference>:", header);
+            Console.WriteLine("Gives you verbose info on a particular setting in the registry\n");
+            Console.WriteLine("\t{0}get [solution] [preference]:", header);
+            Console.WriteLine("Lists the current value of a setting, all settings in a solution, or all settings in the registry, depending on provided parameters\n");
+            Console.WriteLine("\t{0}set <solution> <preference> <value>:", header);
+            Console.WriteLine("Changes the value of a setting, if possible\n");
+            if (cmdline)
+            {
+                return;
+            }
+            Console.WriteLine("\texit:");
+            Console.WriteLine("Ends the program\n");
+        }
+    }
+}
---
+++ Morphic.ManualTesterCLI/RegistryManager.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Morphic.Core;
+using Morphic.Settings;
+using System;
+using System.Threading.Tasks;
+
+namespace Morphic.ManualTesterCLI
+{
+    public class RegistryManager
+    {
+        private SettingsManager manager;
+
+        public RegistryManager()
+        {
+            var services = new ServiceCollection();
+            services.AddLogging();
+            services.AddSingleton<IServiceProvider>(provider => provider);
+            services.AddSingleton<SettingsManager>();
+            var serviceprovider = services.BuildServiceProvider();
+            manager = serviceprovider.GetRequiredService<SettingsManager>();
+        }
+
+        public async Task<bool> Load(string filepath)
+        {
+            await manager.Populate(filepath);
+            return manager.SolutionsById.Count != 0;
+        }
+
+        public void List()
+        {
+            foreach(var solution in manager.SolutionsById.Values)
+            {
+                Console.WriteLine(solution.Id + ":");
+                foreach(var setting in solution.SettingsByName.Values)
+                {
+                    Console.WriteLine("\t" + setting.Name + " [" + setting.Kind.ToString() + "]");
+                }
+            }
+        }
+
+        public void ListSolutions()
+        {
+            foreach(var solution in manager.SolutionsById.Values)
+            {
+                Console.WriteLine(solution.Id);
+            }
+        }
+
+        public void ListSpecific(string solution)
+        {
+            foreach(var sol in manager.SolutionsById.Values)
+            {
+                if(sol.Id == solution)
+                {
+                    Console.WriteLine(sol.Id + ":");
+                    foreach(var setting in sol.SettingsByName.Values)
+                    {
+                        Console.WriteLine("\t" + setting.Name + " [" + setting.Kind.ToString() + "]");
+                    }
+                    return;
+                }
+            }
+            Console.WriteLine("[ERROR]: Solution not found. Please provide list command with a solution in the registry or no parameter to list all settings.");
+        }
+
+        public void Info(string solution, string preference)
+        {
+            //not sure what to do with this command without debugdescription
+            var setting = manager.Get(new Preferences.Key(solution, preference));
+            if(setting != null)
+            {
+                Console.WriteLine(setting.ToString());
+            }
+        }
+
+#nullable enable
+        public async Task Get(string? solution = null)
+        {
+            bool allSolutions = (solution == null);
+            foreach(var sol in manager.SolutionsById.Values)
+            {
+                if(allSolutions || solution == sol.Id)
+                {
+                    Console.WriteLine(sol.Id + ":");
+                    foreach(var setting in sol.SettingsByName.Values)
+                    {
+                        Console.Write("\t" + setting.Name);
+                        await Get(sol.Id, setting.Name);
+                    }
+                }
+            }
+        }
+#nullable disable
+
+        public async Task Get(string solution, string preference)
+        {
+            var value = await manager.Capture(new Preferences.Key(solution, preference));
+            if (value == null) return;
+            var type = manager.Get(new Preferences.Key(solution, preference)).Kind;
+            Console.WriteLine("[" + type.ToString() + "] value: " + value);
+        }
+
+        public async Task Set(string solution, string preference, string value)
+        {
+            var success = await manager.Apply(new Preferences.Key(solution, preference), value);
+            if(success)
+            {
+                Console.WriteLine("Value applied successfully!");
+            }
+            else
+            {
+                Console.WriteLine("Value application failed.");
+            }
+        }
+    }
+}
