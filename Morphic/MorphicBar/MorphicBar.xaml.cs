ed662299366df1d6d0672edfa8497ce5b45e73e7
Christopher - RtF <58520035+christopher-rtf@users.noreply.github.com>
2022-02-21 12:41:10 -0400

    Switch from WinUI 3 to WPF

--- Morphic/MorphicBar/MorphicBar.xaml.cs
+++ Morphic/MorphicBar/MorphicBar.xaml.cs
@@ -21,19 +21,29 @@
 // * Adobe Foundation
 // * Consumer Electronics Association Foundation
 
-namespace Morphic;
+namespace Morphic.MorphicBar;
 
 using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 /// <summary>
-/// Interaction logic for App.xaml
+/// Interaction logic for MorphicBar.xaml
 /// </summary>
-public partial class App : Application
+public partial class MorphicBar : Window
 {
+    public MorphicBar()
+    {
+        InitializeComponent();
+    }
 }
