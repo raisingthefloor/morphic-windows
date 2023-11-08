d5f58b1d95fb30c680ea13062450859cc637a4c5
Christopher - RtF <58520035+christopher-rtf@users.noreply.github.com>
2023-11-08 18:22:59 -0400

    Add auto-update URLs for x86, x64 and arm64

--- Morphic.Client/Config/UpdateOptions.cs
+++ Morphic.Client/Config/UpdateOptions.cs
@@ -3,5 +3,8 @@
     internal class UpdateOptions
     {
         public string AppCastUrl { get; set; } = "";
-    }
+        public string AppCastUrlX86 { get; set; } = "";
+        public string AppCastUrlX64 { get; set; } = "";
+        public string AppCastUrlArm64 { get; set; } = "";
+     }
 }
