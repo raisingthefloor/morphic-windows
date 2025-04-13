2cfc12e69c63d7a0d737b88053beaeb8ed8237c8
Christopher - RtF <58520035+christopher-rtf@users.noreply.github.com>
2025-04-13 13:41:25 -0400

    Update Process and ProcessWatcher to 2.0 code

--- Morphic.WindowsNative/Process/Process.cs
+++ Morphic.WindowsNative/Process/Process.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2021-2023 Raising the Floor - US, Inc.
+﻿// Copyright 2021-2025 Raising the Floor - US, Inc.
 //
 // Licensed under the New BSD license. You may not use this file except in
 // compliance with this License.
@@ -40,86 +40,153 @@ public class Process
           PathIsInvalid,
           UnknownShellExecuteErrorCode,
      }
-     //
-     public static MorphicResult<string, GetPathToExecutableForFileError> GetAssociatedExecutableForFile(string path)
-     {
-          StringBuilder pathToExecutable = new StringBuilder(ExtendedPInvoke.MAX_PATH + 1);
-          var resultCode = ExtendedPInvoke.FindExecutable(path, null, pathToExecutable);
-          if (resultCode.ToInt64() > 32)
-          {
-               // success
-               return MorphicResult.OkResult(pathToExecutable.ToString());
-          }
-          else
-          {
-               // failure
-               switch (resultCode.ToInt64())
-               {
-                    case (Int64)ExtendedPInvoke.ShellExecuteErrorCode.SE_ERR_FNF:
-                         return MorphicResult.ErrorResult(GetPathToExecutableForFileError.FileNotFound);
-                    case (Int64)ExtendedPInvoke.ShellExecuteErrorCode.SE_ERR_PNF:
-                         return MorphicResult.ErrorResult(GetPathToExecutableForFileError.PathIsInvalid);
-                    case (Int64)ExtendedPInvoke.ShellExecuteErrorCode.SE_ERR_ACCESSDENIED:
-                         return MorphicResult.ErrorResult(GetPathToExecutableForFileError.AccessDenied);
-                    case (Int64)ExtendedPInvoke.ShellExecuteErrorCode.SE_ERR_OOM:
-                         return MorphicResult.ErrorResult(GetPathToExecutableForFileError.OutOfMemoryOrResources);
-                    case (Int64)ExtendedPInvoke.ShellExecuteErrorCode.SE_ERR_NOASSOC:
-                         return MorphicResult.ErrorResult(GetPathToExecutableForFileError.NoAssociatedExecutable);
-                    default:
-                         return MorphicResult.ErrorResult(GetPathToExecutableForFileError.UnknownShellExecuteErrorCode);
-               }
-          }
-     }
+    //
+    public static MorphicResult<string, GetPathToExecutableForFileError> GetAssociatedExecutableForFile(string path)
+    {
+        var pathToExecutableLength = Windows.Win32.PInvoke.MAX_PATH + 1;
 
-     public static IEnumerable<IntPtr> GetAllWindowHandlesForProcess(int processId)
-     {
-          var handles = new List<IntPtr>();
+        string pathToExecutable;
+        nint findExecutableResultAsNint;
+        unsafe
+        {
+            fixed (char* pathToExecutableAsUnsafeChars = new char[(int)pathToExecutableLength])
+            {
+                // see: https://learn.microsoft.com/en-us/windows/win32/api/shellapi/nf-shellapi-findexecutablew
+                var findExecutableResult = Windows.Win32.PInvoke.FindExecutable(path, null, pathToExecutableAsUnsafeChars);
+                if (findExecutableResult is not null && findExecutableResult!.DangerousGetHandle() > 32)
+                {
+                    // success
+                    // NOTE: we are relying on FindExecutable to return a valid null-terminated string
+                    pathToExecutable = new string(pathToExecutableAsUnsafeChars);
+                    return MorphicResult.OkResult(pathToExecutable);
+                }
+                else
+                {
+                    // failure
+                    // capture failure (result) code
+                    findExecutableResultAsNint = findExecutableResult!.DangerousGetHandle();
+                }
+            }
+        }
+        //
+        // if we reach here, we have a failure result
+        //
+        // failure
+        switch (findExecutableResultAsNint)
+        {
+            case (nint)Windows.Win32.PInvoke.SE_ERR_FNF:
+                return MorphicResult.ErrorResult(GetPathToExecutableForFileError.FileNotFound);
+            case (nint)Windows.Win32.PInvoke.SE_ERR_PNF:
+                return MorphicResult.ErrorResult(GetPathToExecutableForFileError.PathIsInvalid);
+            case (nint)Windows.Win32.PInvoke.SE_ERR_ACCESSDENIED:
+                return MorphicResult.ErrorResult(GetPathToExecutableForFileError.AccessDenied);
+            case (nint)Windows.Win32.PInvoke.SE_ERR_OOM:
+                return MorphicResult.ErrorResult(GetPathToExecutableForFileError.OutOfMemoryOrResources);
+            case (nint)Windows.Win32.PInvoke.SE_ERR_NOASSOC:
+                return MorphicResult.ErrorResult(GetPathToExecutableForFileError.NoAssociatedExecutable);
+            default:
+                return MorphicResult.ErrorResult(GetPathToExecutableForFileError.UnknownShellExecuteErrorCode);
+        }
+    }
 
-          var threadsForProcess = System.Diagnostics.Process.GetProcessById(processId).Threads;
-          foreach (var threadAsObject in threadsForProcess)
-          {
-               ProcessThread? thread = threadAsObject as ProcessThread;
-               if (thread is not null)
-               {
-                    // NOTE: as we control the enumeration callback and it always returns true, we do not capture or analyze any errors returned by this function call;
-                    // //    a 'false' response here would theoretically just mean that there were no windows to enumerate (in which case we'd correctly return an empty list)
-                    _ = WindowsApi.EnumThreadWindows(thread.Id, (hWnd, lParam) => { handles.Add(hWnd); return true; }, IntPtr.Zero);
-               }
-          }
+    public interface IGetAllWindowHandlesForProcessError
+    {
+        public record NotRunning : IGetAllWindowHandlesForProcessError;
+    }
+    public static MorphicResult<IEnumerable<IntPtr>, IGetAllWindowHandlesForProcessError> GetAllWindowHandlesForProcess(int processId)
+    {
+        List<IntPtr> handles = [];
+        List<int> failedProcessThreads = [];
 
-          return handles;
-     }
+        System.Diagnostics.ProcessThreadCollection? threadsForProcess;
+        try
+        {
+            threadsForProcess = System.Diagnostics.Process.GetProcessById(processId).Threads;
+        }
+        catch (ArgumentException)
+        {
+            return MorphicResult.ErrorResult<IGetAllWindowHandlesForProcessError>(new IGetAllWindowHandlesForProcessError.NotRunning());
+        }
+        //
+        foreach (var threadAsObject in threadsForProcess)
+        {
+            ProcessThread? thread = threadAsObject as ProcessThread;
+            if (thread is not null)
+            {
+                // NOTE: as we control the enumeration callback and it always returns true, we do not capture or analyze any errors returned by this function call;
+                //       a 'false' response here just means that there were no windows to enumerate (in which case we'd correctly return an empty list)
+                // see: https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-enumthreadwindows
+                _ = Windows.Win32.PInvoke.EnumThreadWindows((uint)thread.Id, (hWnd, lParam) => { handles.Add(hWnd); return true; }, IntPtr.Zero);
+            }
+        }
 
-     // NOTE: This function returns MorphicResult.ErrorResult() even if some windows were closed; we may want to add granularity (i.e. "CompletelyFailed" vs "PartiallyFailed" vs "Success") in the future
-     public static MorphicResult<MorphicUnit, MorphicUnit> CloseAllWindowsForProcess(int processId)
-     {
-          var success = true;
+        return MorphicResult.OkResult<IEnumerable<IntPtr>>(handles);
+    }
 
-          var windowHandlesForProcess = GetAllWindowHandlesForProcess(processId);
-          foreach (var handle in windowHandlesForProcess)
-          {
-               var result = WindowsApi.SendNotifyMessage(handle, WindowsApi.WindowMessages.WM_CLOSE, UIntPtr.Zero, IntPtr.Zero);
-               if (result != false)
-               {
-                    success = false;
-               }
-          }
+    public interface ICloseAllWindowsError
+    {
+        public record NotRunning: ICloseAllWindowsError;
+        public record PartialFailure(List<IntPtr> SuccessWindowHandles, List<KeyValuePair<IntPtr, IWin32ApiError>> FailureWindows) : ICloseAllWindowsError;
+    }
+    // NOTE: This function returns a list of closed window handles upon success (or a list of successfully/unsuccessfully closed window handles on partial failure)
+    public static MorphicResult<List<IntPtr>, ICloseAllWindowsError> CloseAllWindowsForProcess(int processId)
+    {
+        IEnumerable<IntPtr> windowHandlesForProcess = [];
+        //
+        var windowHandlesForProcessResult = GetAllWindowHandlesForProcess(processId);
+        if (windowHandlesForProcessResult.IsError)
+        {
+            switch (windowHandlesForProcessResult.Error!)
+            {
+                case IGetAllWindowHandlesForProcessError.NotRunning:
+                    break;
+                default:
+                    throw new MorphicUnhandledErrorException();
+            }
+        }
+        windowHandlesForProcess = windowHandlesForProcessResult.Value!;
+        //
+        List<IntPtr> closedWindowHandles = [];
+        List<KeyValuePair<IntPtr, IWin32ApiError>> failureWindowHandles = [];
+        foreach (var handle in windowHandlesForProcess)
+        {
+            var hwnd = new Windows.Win32.Foundation.HWND(handle);
+            //
+            // see: https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-sendnotifymessagew
+            var sendNotifyMessageResult = Windows.Win32.PInvoke.SendNotifyMessage(hwnd, Windows.Win32.PInvoke.WM_CLOSE, 0, 0);
+            if (sendNotifyMessageResult != 0)
+            {
+                closedWindowHandles.Add(handle);
+            }
+            else
+            {
+                var win32Error = System.Runtime.InteropServices.Marshal.GetLastWin32Error();
+                failureWindowHandles.Add(new KeyValuePair<nint, IWin32ApiError>(handle, new IWin32ApiError.Win32Error((uint)win32Error)));
+            }
+        }
 
-          return success ? MorphicResult.OkResult() : MorphicResult.ErrorResult();
-     }
+        if (failureWindowHandles.Count > 0)
+        {
+            return MorphicResult.ErrorResult<ICloseAllWindowsError>(new ICloseAllWindowsError.PartialFailure(closedWindowHandles, failureWindowHandles));
+        }
+        else
+        {
+            return MorphicResult.OkResult(closedWindowHandles);
+        }
+    }
 
-     public static string[] GetCurrentProcessNames()
-     {
-          List<string> result = new();
+    public static List<string> GetCurrentProcessNames()
+    {
+        List<string> result = new();
 
-          var processes = System.Diagnostics.Process.GetProcesses();
-          foreach (var process in processes)
-          {
-               // NOTE: we do not check process.HasExited here, as we should never get any exited processes in a fresh process list (and checking the property will consume unnecessary processor cycles)
-               var processName = process.ProcessName;
-               result.Add(processName);
-          }
+        var processes = System.Diagnostics.Process.GetProcesses();
+        foreach (var process in processes)
+        {
+            // NOTE: we do not check process.HasExited here, as we should never get any exited processes in a fresh process list (i.e. checking the property would consume unnecessary processor cycles)
+            var processName = process.ProcessName;
+            result.Add(processName);
+        }
 
-          return result.ToArray();
-     }
+        return result;
+    }
 }
