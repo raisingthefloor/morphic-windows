cd7f131223b212fdbb604cdf5ef2439f0fea7d59
Christopher - RtF <58520035+christopher-rtf@users.noreply.github.com>
2026-03-16 18:53:29 -0400

    Debug startup xaml error

---
+++ Morphic/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+using Morphic;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace Morphic;
+
+public class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        WinRT.ComWrappersSupport.InitializeComWrappers();
+
+        try
+        {
+            Application.Start(p =>
+            {
+                var context = new DispatcherQueueSynchronizationContext(
+                    DispatcherQueue.GetForCurrentThread());
+                SynchronizationContext.SetSynchronizationContext(context);
+                _ = new App();
+            });
+        }
+        catch (Exception ex)
+        {
+            File.WriteAllText(@"C:\temp\startup-crash.log", ex.ToString());
+            throw;
+        }
+    }
+}
